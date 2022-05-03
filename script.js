a325564f50db4f9cea5649dc8cd12034846ac34c
Lee Castillo Arenas <leecastilloare@MacBook-Pro.local>
2022-05-03 14:53:39 -0400

    I am updating my branch with today's code

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 "use strict";
 
-// EASY (1) 
+////////////////////////////// THE BELOW IS FOR THE EASY (1) SECTION OF THE ASSIGNMENT
+//// DRIVER: LEE     NAVIAGATOR: BELLUL
+
 // Write a JavaScript function that takes in a random array and outputs two new arrays of odd numbers and even 
 // numbers.
 // Example:
@@ -10,7 +12,6 @@
 
 
 const ranNum = [2, 3, 4, 5, 33, 33, 212, 11, 33];
-
 
 const evenOddFunction = function (ranNum) {
 
@@ -28,11 +29,36 @@ const evenOddFunction = function (ranNum) {
 }
 
 
-
-
-evenOddFunction(ranNum)
-console.log(evens);
-console.log(odds);
+// evenOddFunction(ranNum)
+// console.log(evens);
+// console.log(odds);
 
 const evens = [];
 const odds = [];
+
+///////////////////////////////// BELOW IS THE MEDIUM (1) SECTION OF THE ASSIGNMENT
+//// DRIVER: LEE     NAVIAGATOR: BELLUL
+
+// MEDIUM(1)
+// Create a Function Called “vowelChecker” that takes in a single argument (x) and logs weather or not the input
+// is a vowel
+// Example 1:
+// Input: vowelChecker(‘b’)
+// Output: ‘This is not a vowel’
+// Example 2:
+// Input: vowelChecker(‘a’)
+// Output: ‘This is a vowel’
+
+function isVowel(str) {
+    const value = ["a", "e", "i", "o", "u"].indexOf(str.toLowerCase()) !== -1
+
+    if (value) {
+        return console.log(`This letter: ${str} is a vowel`);
+
+    } else {
+        return console.log(`This letter: ${str} is NOT a vowel`);
+    }
+}
+
+isVowel("o");
+
