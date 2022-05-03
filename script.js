b53ea5c1a2d4119fa646731e34654a915f52f5bd
Lee Castillo Arenas <leecastilloare@MacBook-Pro.local>
2022-05-03 16:01:16 -0400

    Pushing the third medium to the repo

--- script.js
+++ script.js
@@ -48,19 +48,19 @@ const odds = [];
 // Input: numArray = [10, 18, 19, 29, 33, 35, 47, 66, 83] 
 // Output: primeArray = [19 ,29 ,47 , 83]
 
-let num = [ 3, 11, 29, 82, 4, 84, 36, 53, 47]
+let num = [3, 11, 29, 82, 4, 84, 36, 53, 47]
 
-const checkPrime = num.filter(function(number) {
-  if (number <= 1) {
-    return false;
-  } else {
-    for (let i = 2; i < number; i++) {
-      if (number % i == 0) {
+const checkPrime = num.filter(function (number) {
+    if (number <= 1) {
         return false;
-      }
+    } else {
+        for (let i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
-    return true;
-  }
 });
 
 console.log(checkPrime)
@@ -78,18 +78,18 @@ console.log(checkPrime)
 // Input: vowelChecker(‘a’)
 // Output: ‘This is a vowel’
 
-function isVowel(str) {
-    const value = ["a", "e", "i", "o", "u"].indexOf(str.toLowerCase()) !== -1
+// function isVowel(str) {
+//     const value = ["a", "e", "i", "o", "u"].indexOf(str.toLowerCase()) !== -1
 
-    if (value) {
-        return console.log(`This letter: ${str} is a vowel`);
+//     if (value) {
+//         return console.log(`This letter: ${str} is a vowel`);
 
-    } else {
-        return console.log(`This letter: ${str} is NOT a vowel`);
-    }
-}
+//     } else {
+//         return console.log(`This letter: ${str} is NOT a vowel`);
+//     }
+// }
 
-isVowel("o");
+// isVowel("o");
 
 ///////////////////////////////// BELOW IS THE MEDIUM (2) SECTION OF THE ASSIGNMENT
 //// DRIVER: BELLUL     NAVIAGATOR: LEE
@@ -105,20 +105,57 @@ isVowel("o");
 //   String 2: Dogs eat ants
 // Output: False
 
-const anagram = function(str1, str2) {
-    // let len1 = str1.length;
-    // let len2 = str2.length;
-    // if(len1 !== len2) {
-    //     console.log('Invalid Input')
-    //     return
-    // }
-    let string1 = str1.split('').sort().join('');
-    let string2 = str2.split('').sort().join('');
-    if(string1 === string2){
-        console.log('True');
-    } else {
-        console.log('False');
+// const anagram = function (str1, str2) {
+//     // let len1 = str1.length;
+//     // let len2 = str2.length;
+//     // if(len1 !== len2) {
+//     //     console.log('Invalid Input')
+//     //     return
+//     // }
+//     let string1 = str1.split('').sort().join('');
+//     let string2 = str2.split('').sort().join('');
+//     if (string1 === string2) {
+//         console.log('True');
+//     } else {
+//         console.log('False');
+//     }
+// }
+
+// anagram('indian', 'ndiian')
+
+
+///////////////////////////////// BELOW IS THE MEDIUM (3) SECTION OF THE ASSIGNMENT //////////////////////////
+//// DRIVER: Lee     NAVIAGATOR: BELLUL
+
+// MEDIUM(3)
+// Write a function that takes in two numbers and determines the largest positive integer that divides the two
+// numbers without a remainder.
+// Example 1:
+// Input: gdc_two_numbers(336,360)
+// Output: 24
+// Example 2:
+// Input: gdc_two_numbers(78,126)
+// Output: 6
+
+
+const biggestRemainder = function (num1, num2) {
+
+    var x = Math.abs(num1);
+    var y = Math.abs(num2);
+
+
+    if ((typeof x !== "number") || (typeof y != "number")) {
+        return false;
     }
+
+    while (y) {
+        var e = y;
+        y = x % y;
+        x = e
+    }
+
+    return x
+
 }
 
-anagram('indian' , 'ndiian')
+console.log(biggestRemainder(336, 360));
